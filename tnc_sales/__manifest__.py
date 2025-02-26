3facf4a7abba37ec709d56ac15f3d27d4ae43977
SinauGit <newprogrammerjava@gmail.com>
2025-02-26 14:13:45 +0700

    tambah modul tnc_sales

---
+++ tnc_sales/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "",
+    'version': '16.0.1.0',
+    'author': "",
+    'website': "",
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_view.xml',
+    ],
+    'sequence': 1,
+    'installable': True,
+    'application': True,
+}
