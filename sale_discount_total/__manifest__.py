18cb2b8aa469c85d4358a25599fe2bc4fa27ce68
SinauGit <newprogrammerjava@gmail.com>
2025-03-08 11:55:11 +0700

    tambah modul sale_discount_total dan revisi so 2 file

---
+++ sale_discount_total/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Sale Discount',
+    'version': '16.0.1.1.1',
+    'author': '',
+    'description':'',
+    'depends': ['sale','account'],
+    'data': [
+        # 'views/res_config_view.xml',
+        'views/sale_view.xml',
+        # 'views/account_invoice_view.xml',
+        'views/invoice_report.xml',
+        # 'views/sale_order_report.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
