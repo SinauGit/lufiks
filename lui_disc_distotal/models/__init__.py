cd554a68c79bc0d68adcecc6a759152146f00a18
SinauGit <newprogrammerjava@gmail.com>
2025-09-09 01:03:32 +0700

    tambah modul lui_disc_distotal

---
+++ lui_disc_distotal/models/__init__.py
@@ -0,0 +1,6 @@
+# from . import discount_approval
+from . import sale
+from . import account_invoice
+from . import sale_order_line
+from . import account_tax
+from . import account_move_line
--- report_pdf_options/__manifest__.py
+++ report_pdf_options/__manifest__.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Pdf report options',
-    'summary': """shows a modal window with options for printing, downloading or opening pdf reports""",
-    'description': """
-        Choose one of the following options when printing a pdf report:
-        - print. print the pdf report directly with the browser
-        - download. download the pdf report on your computer
-        - open. open the pdf report in a new tab
-        You can also set a default options for each report
-    """,
-    'author': 'Luis Rodrigo Mejia Mateus',
-    'category': 'Productivity',
-    'images': ['images/main_1.png', 'images/main_screenshot.png'],
+    'name': 'report options',
+    'author': 'Luis Rodrigo Port dan Revise By Rizky',
     'depends': ['web'],
     'data': [
         'views/ir_actions_report.xml',
