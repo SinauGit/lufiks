3e8c47de7534ec84076c432c7c2f666d1b58b100
SinauGit <newprogrammerjava@gmail.com>
2026-02-05 17:18:18 +0700

    vat jadi npwp

--- vat_custom/models/res_partner.py
+++ vat_custom/models/res_partner.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    vat_custom = fields.Char(string='VAT')
+    vat_custom = fields.Char(string='NPWP')
